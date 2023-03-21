989e5fc8ad8a0648b9b0300e850b049291cf3313
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2023-03-21 12:18:46 +0200

    Do the modal logic

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function closeModal() {
+  var modal = document.getElementById("claim_module");
+  modal.style.display = "none";
+}
+
+function openModal() {
+  var modal = document.getElementById("claim_module");
+  modal.style.display = "block";
+}
